9dd58f58479ecf62973aa1db65aaf0a374f8fccb
MVV023 <miguelveravega@gmail.com>
2016-11-08 08:44:17 -0400

    Update hola.py

--- hola.py
+++ hola.py
@@ -1,7 +1,8 @@
 print "Hola"
-# Maria Fernanda aportando
-Def factorial(n):
-  f=1
-  for in range (1,n+1)
-    f*=1
-  Return f
+def sumar(x, y):
+    return x + y
+
+print sumar(3, 2)
+
+def division(x,z):
+    return x/z
